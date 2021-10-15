fbd2064a01b6e39b98ff7c391affac90959db776
KaranBohara <karanrockbohara@gmail.com>
2021-10-15 12:04:51 +0530

    feat:search box updated

--- src/components/Search_space/Searchpage.tsx
+++ src/components/Search_space/Searchpage.tsx
@@ -22,11 +22,11 @@ const Searchpage=()=>
         {
             bestPrice = item.Price - ((item.Discount * item.Price) / 100);
         return(<div className="row" style={{border:"1px solid rgb(240, 240, 240)"}}>
-            <div className="col-3 d-flex align-items-center justify-content-center" style={{height:"8rem",borderRight:"1px solid rgb(240, 240, 240)"}}><img src={item.src}></img></div>
+            <div className="col-3 d-flex align-items-center justify-content-center" style={{height:"9rem",borderRight:"1px solid rgb(240, 240, 240)"}}><img src={item.src}></img></div>
             <div className="col-9">
-                <div className="row"><div className="product-name" style={{marginTop:"1rem",height:"fit-content"}}>{item.MedicineName}</div></div>
-                <div className="row"><div className="product-manufacturer" style={{marginTop:".3rem",height:"fit-content"}}>{item.Manufacturers}</div></div>
-                <div className="row"><div className="product-bestprice" style={{marginTop:".3rem",height:"fit-content"}}>Rs.{bestPrice}</div></div>
+                <div className="row"><div className="product-name" style={{marginTop:"1rem",height:"2.5rem"}}>{item.MedicineName}</div></div>
+                <div className="row"><div className="product-manufacturer" style={{marginTop:".3rem",height:"1.3rem"}}>{item.Manufacturers}</div></div>
+                <div className="row"><div className="product-bestprice" style={{marginTop:".3rem",height:"1.3rem"}}>Rs.{bestPrice}</div></div>
                 <div className="row shopping-bag"><img src="https://www.linkpicture.com/q/add-to-basket.png" ></img></div>
             </div>
         </div>);})
